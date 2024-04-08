ed4c87bcbc570798311f10daf2a6b5eb7ff94d9f
Saksham <sambhavgupta295@gmail.com>
2024-04-08 19:06:55 +0530

    context-api-1

---
+++ miniContext/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import UserContextProvider from './context/UserContextProvider'
+import Login from './components/Login'
+import Profile from './components/Profile'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <UserContextProvider>
+      <h1>React with Chai and share is important</h1>
+      <Login></Login>
+      <Profile/>
+    </UserContextProvider>
+  )
+}
+
+export default App
