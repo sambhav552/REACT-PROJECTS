50595c6b6a0ff6ede1c127c5d13da32e678a6c81
Saksham <sambhavgupta295@gmail.com>
2024-04-17 23:14:06 +0530

    env change

--- MegaBlog/src/App.jsx
+++ MegaBlog/src/App.jsx
@@ -5,7 +5,7 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  console.log(import.meta.env.VITE_APPWRITE_URL);
   return (
     <>
       <h1>A blog page with appwrite</h1>
