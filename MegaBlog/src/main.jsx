74e161bf72d79e3d321d2d0a5d79df870aef1bad
Saksham <sambhavgupta295@gmail.com>
2024-04-19 09:09:44 +0530

    imports

--- MegaBlog/src/main.jsx
+++ MegaBlog/src/main.jsx
@@ -2,9 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { Provider } from 'react-redux'
+import store from './store/store.js'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import { AuthLayout, Login } from './components/index.js'
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Provider store={store}>
+
     <App />
+    </Provider>
+    
   </React.StrictMode>,
 )
